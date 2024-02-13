78417d00f183899f2dfe0ae59618095aad11dcd4
correnoc <clement.correnoz@grenoble-inp.org>
2024-02-13 12:42:59 +0100

    login pas très complet

---
+++ src/utils/i18n.js
@@ -0,0 +1,53 @@
+const fr = {
+  GREETINGS: {
+    SIGNUP: "S'inscrire",
+    LOGIN: "Se connecter",
+  },
+
+  FORM_FIELDS: {
+    NAME: "Nom",
+    EMAIL: "E-mail",
+    PASSWORD: "Mot de passe",
+    RETYPE_PASSWORD: "Confirmer le mot de passe",
+    SIGNUP: "S'inscrire",
+    LOGIN: "Se connecter",
+    SIGNUP_ALT_TEXT: "Déjà un compte ?",
+    LOGIN_ALT_TEXT: "Nouvel Utilisateur ?",
+  },
+
+  ERRORS: {
+    EMPTY_EMAIL: "Email requis",
+    INVALID_EMAIL: "Email invalide",
+    EMPTY_PASSWORD: "Mot de passe requis",
+    PASSWORD_MISMATCH: "Les mots de passe ne correspondent pas",
+    AUTH: "Email/Mot de passe invalide",
+  },
+};
+
+const en = {
+  GREETINGS: {
+    SIGNUP: "Sign Up",
+    LOGIN: "Login",
+  },
+
+  FORM_FIELDS: {
+    NAME: "Name",
+    EMAIL: "E-mail",
+    PASSWORD: "Password",
+    RETYPE_PASSWORD: "Retype Password",
+    SIGNUP: "Sign Up",
+    LOGIN: "Login",
+    SIGNUP_ALT_TEXT: "Existing user?",
+    LOGIN_ALT_TEXT: "New user?",
+  },
+
+  ERRORS: {
+    EMPTY_EMAIL: "Email is required",
+    INVALID_EMAIL: "Invalid email",
+    EMPTY_PASSWORD: "Password is required",
+    PASSWORD_MISMATCH: "Passwords do not match",
+    AUTH: "Incorrect email/password",
+  },
+};
+
+export default fr;
