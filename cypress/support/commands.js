b774c67accd4b41c623a641fe77dae58dc767ee2
Alexis Sanson <alexis-erwan-joachim.sanson@grenoble-inp.org>
2024-02-27 14:17:17 +0100

    Tests : premiers tests mis en place
    Cela concerne le fichier home_page/home.cy.js qui
    tente de connecter un utilisateur à l'application
    
    Doit être commenté et enrichi

---
+++ cypress/support/commands.js
@@ -0,0 +1,40 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import firebase from "firebase/app"
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.get("input[name='email']").type(email);
+  cy.get("input[name='password']").type(password);
+});
+
+Cypress.Commands.add("signup", (firstName, name, email, password1, password2) => {
+  cy.get("form").find("input[name='name']").type(firstName);
+  cy.get("form").find("input[name='surnamename']").type(name);
+  cy.get("form").find("input[name='email']").type(email);
+  cy.get("form").find("input[name='password']").type(password1);
+  cy.get("form").find("input[name='retype']").type(password2);
+});
+
