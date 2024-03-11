4da0e8de8c1ab237dc72064d0d0365620a8e5a46
Hippolyte <Hippolyte@bois2scie.com>
2024-03-11 21:18:27 +0100

    joining button style

--- src/components/groupMembership.js
+++ src/components/groupMembership.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { FaLock, FaUnlock } from "react-icons/fa";
-import { isUserInGroup, joinGroup } from "../utils/firebase";
+import { isUserInGroup, joinGroup, leaveGroup } from "../utils/firebase";
+import "../styles/groupMembership.css";
 
 const GroupMembership = ({ group, userSchool }) => {
   const [isInGroup, setIsInGroup] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   useEffect(() => {
     const checkMembership = async () => {
@@ -21,13 +23,36 @@ const GroupMembership = ({ group, userSchool }) => {
   }, [group.id]);
 
   const handleJoinGroup = () => {
+    setIsTransitioning(true);
     joinGroup(group.id)
       .then(() => {
         console.log("Vous avez rejoint le groupe avec succès !");
         setIsInGroup(true);
+        // Délai de 2 secondes (2000 millisecondes) avant de désactiver l'état de transition
+        setTimeout(() => {
+          setIsTransitioning(false);
+        }, 2000);
       })
       .catch((error) => {
         console.error("Erreur lors de la tentative de rejoindre le groupe :", error);
+        setIsTransitioning(false);
+      });
+  };
+  
+  const handleLeaveGroup = () => {
+    setIsTransitioning(true);
+    leaveGroup(group.id)
+      .then(() => {
+        console.log("Vous avez quitté le groupe avec succès !");
+        setIsInGroup(false);
+        // Délai de 2 secondes (2000 millisecondes) avant de désactiver l'état de transition
+        setTimeout(() => {
+          setIsTransitioning(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la tentative de quitter le groupe :", error);
+        setIsTransitioning(false);
       });
   };
 
@@ -36,11 +61,21 @@ const GroupMembership = ({ group, userSchool }) => {
   }
 
   if (group.isPublic || group.school === userSchool) {
-    if (isInGroup) {
-      return <div>Rejoint</div>;
+    if (isTransitioning) {
+      return (
+            <button className="disabled-group-button" onClick={handleLeaveGroup}>
+                {isInGroup ? "Rejoint" : "Quitté"}
+            </button>
+            );
+    } else if (isInGroup) {
+      return (
+        <button className="leave-group-button" onClick={handleLeaveGroup}>
+          Quitter le groupe
+        </button>
+      );
     } else {
       return (
-        <button onClick={handleJoinGroup}>
+        <button className="join-group-button" onClick={handleJoinGroup}>
           Rejoindre
         </button>
       );
