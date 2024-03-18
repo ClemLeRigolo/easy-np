2175abf6c165d749cd752a60e88128d57e2f2514
Alexis Sanson <alexis-erwan-joachim.sanson@grenoble-inp.org>
2024-03-18 16:25:45 +0100

    Test : test des posts

--- cypress/e2e/post/create_post.cy.js
+++ cypress/e2e/post/create_post.cy.js
@@ -6,6 +6,10 @@ context('Post from differents schools', () => {
       cy.get("form").submit();
     });
 
+    beforeEach(() => {
+      cy.visit("/");
+    })
+
     it("View the default posts", () => {
       cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !").contains("Ensimag Ensimag");
       cy.getPostByText("Mon emblem est le Phénix").contains("Phelma Phelma");
@@ -13,7 +17,11 @@ context('Post from differents schools', () => {
     })
 
     it("clicking on likes", () => {
-
+      const post = cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !");
+      post.find(".post-like-btn").click();
+      post.contains(" 4 Likes");
+      post.get(".liked").click();
+      post.contains(" 3 Likes");
     })
 
     after(() => {
