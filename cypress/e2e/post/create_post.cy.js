48f1ecbb658d69068d064220a773f36f4751bcee
Alexis Sanson <alexis-erwan-joachim.sanson@grenoble-inp.org>
2024-04-01 09:37:58 +0200

    Test sur les posts

--- cypress/e2e/post/create_post.cy.js
+++ cypress/e2e/post/create_post.cy.js
@@ -1,32 +1,47 @@
 context('Post from differents schools', () => {
-  describe("1. Connection avec l'ensimag", () => {
-    beforeEach(() => {
-      cy.visit("/login");
-      cy.login("ensimag@grenoble-inp.fr", "Password!");
-      cy.get("form").submit();
+  before(() => {
+    cy.visit("/login");
+    cy.login("ensimag@grenoble-inp.fr", "Password!");
+    cy.get("form").submit();
+  });
+
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("View the default posts", () => {
+    cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !").contains("Ensimag Ensimag");
+    cy.getPostByText("Mon emblem est le Phénix").contains("Phelma Phelma");
+  })
+
+  it("clicking on likes", () => {
+    const post = cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !");
+    post.within(() => {
+      const buttonLike = post.get("[data-cy='like']");
+      buttonLike.should("have.attr", "likes", "3");
+      cy.get("button[data-cy='like']").click();
+      buttonLike.should("have.attr", "likes", "4");
+      cy.get("button[data-cy='like']").click();
     });
+  })
 
-    it("View the default posts", () => {
-      cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !").contains("Ensimag Ensimag");
-      cy.getPostByText("Mon emblem est le Phénix").contains("Phelma Phelma");
-      cy.get(".post").should("have.length", 2);
-    })
+  it("clicking on comments", () => {
+    const post = cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !");
+    post.within(() => {
+      post.contains("Phelma n'est pas loin derrière !").should("not.exist");
+      post.get("[data-cy='toggleComments']").click();
+      post.get("[data-cy='comments']");
+    });
+  })
 
-    it("clicking on likes", () => {
-      const post = cy.getPostByText("Ensimag est l'une des meilleurs écoles d'ingénieur de France !");
-      cy.intercept("*", "patch").as("click");
-      post.find(".post-like-btn").click();
-      post.contains(" 4 Likes");
-    })
+  // it("Creating a post", () => {
+  //   const postArea = cy.get(".postInput");
+  //   postArea.find(".post-input").type("A new post !");
+  //   cy.get("button[class='.post-submit-btn']");
+  // })
 
-    // it("Creating a post", () => {
-    //   const postArea = cy.get(".postInput");
-    //   postArea.find(".post-input").type("A new post !");
-    //   cy.get("button[class='.post-submit-btn']");
-    // })
-
-    afterEach(() => {
-      cy.get("#signout").click();
-    })
+  after(() => {
+    visit("/");
+    cy.get("#signout").click();
   });
 });
